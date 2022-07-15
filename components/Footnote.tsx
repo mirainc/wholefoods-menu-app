5d0f0e805d083887f9a7b95c0801e05c95186582
Sebastian Key <sebastianwkey@gmail.com>
2022-07-15 11:02:44 +1000

    Add comment

--- components/Footnote.tsx
+++ components/Footnote.tsx
@@ -1,5 +1,6 @@
 const Footnote = ({ children }: React.PropsWithChildren) => {
   return (
+    // Ensure new line characters are respected by setting white space to "pre-wrap"
     <p style={{ whiteSpace: "pre-wrap" }} className="text-turquoise px-12 py-8 text-4xl">
       {children}
     </p>
